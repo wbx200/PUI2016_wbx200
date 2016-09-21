7be3b8d042e8ffb18de32ab9c0742c142e8b3977
wbx200 <william.b.xia@gmail.com>
2016-09-21 07:26:10 -0400

    Update get_bus_info_wbx200.py

--- HW2_wbx200/get_bus_info_wbx200.py
+++ HW2_wbx200/get_bus_info_wbx200.py
@@ -5,6 +5,10 @@ import os
 import sys
 import csv
 
+headers = ['Latitude', 'Longitude', 'Stop Name', 'Stop Status']
+writer = csv.writer(open(sys.argv[3], 'w'))
+writer.writerow(headers)
+
 url = "http://bustime.mta.info/api/siri/vehicle-monitoring.json?key=" + sys.argv[1] + "&VehicleMonitoringDetailLevel=calls&LineRef=" + sys.argv[2]
 
 response = urllib2.urlopen(url)
@@ -15,8 +19,8 @@ position = data['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['Vehi
 
 busline = 'Bus line : ' + sys.argv[2]
 numberofbuses = len(position)
-print (busline)
-print ('Number of Active Buses :', numberofbuses)
+#print (busline)
+#print ('Number of Active Buses :', numberofbuses)
 a = 0
 for i in position:
     latitude = i['MonitoredVehicleJourney']['VehicleLocation']['Latitude']
@@ -29,11 +33,15 @@ for i in position:
         status = i['MonitoredVehicleJourney']['OnwardCalls']['OnwardCall'][0]['Extensions']['Distances']['PresentableDistance']
     except KeyError:
         status = "N/A"
-    print ('Bus', a, 'is at latitude', latitude, 'and longitude', longitude, stopname, status)
-    a = a + 1
+a = a + 1
 
-outfile_path='/home/CUSP/wbx200/' + sys.argv[2] +'.csv'
-writer = csv.writer(open(outfile_path, 'w'))
-headers = ['Latitude', 'Longitude', 'Stop Name', 'Stop Status']
-writer.writerow(headers)
-
+b = 0 
+for i in position:
+    row = []
+    row.append(str(i['MonitoredVehicleJourney']['VehicleLocation']['Latitude']))
+    row.append(str(i['MonitoredVehicleJourney']['VehicleLocation']['Longitude']))
+    row.append(str(i['MonitoredVehicleJourney']['OnwardCalls']['OnwardCall'][0]['StopPointName']))
+    row.append(str(i['MonitoredVehicleJourney']['OnwardCalls']['OnwardCall'][0]['Extensions']['Distances']['PresentableDistance']))
+    writer.writerow(row)
+    b = b + 1
+        #    print ('Bus', a, 'is at latitude', latitude, 'and longitude', longitude, #stopname, status)
