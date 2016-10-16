e2a081229fbb7fa50fbebe91b4af568264e49ef1
wbx200 <william.b.xia@gmail.com>
2016-10-15 21:52:21 -0400

    Delete PUIDATA.py

--- HW6_wbx200/PUIDATA.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def getEnergy():
-    def getzip():
-    os.system("mkdir " + os.getenv("PUIDATA")+"/Manhattan")
-    os.system("curl -O https://www1.nyc.gov/assets/planning/download/zip/data-maps/open-data/mn_mappluto_16v1.zip")
-    os.system("mv " + "mn_mappluto_16v1.zip " + os.getenv("PUIDATA")+"/Manhattan")
-    os.system("unzip " + os.getenv("PUIDATA") + "/Manhattan/" + "mn_mappluto_16v1.zip -d " + os.getenv("PUIDATA")+"/Manhattan")
-    os.system("rm " + os.getenv("PUIDATA") + "/Manhattan/mn_mappluto_16v1.zip")
